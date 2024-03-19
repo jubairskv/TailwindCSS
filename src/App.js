8b7f641bb897c9291752108a7e076923be72c618
jubairskv <jubairskv@gmail.com>
2024-03-19 13:23:46 +0530

    words chnaged

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div >
      <h1 className="text-3xl font-bold underline border ">Hello world!</h1>
-     <h2>new line added</h2>
+     <h2>new line add to git hub</h2>
       <h3>jubair</h3>
     </div>
   );
