769a50ffe09f64245fe2156d5f7377d76abf42ce
jubairskv <jubairskv@gmail.com>
2024-03-19 13:03:00 +0530

    new code added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@
 function App() {
   return (
     <div >
-     <h1 className="text-3xl font-bold underline border ">
-      Hello world!
-    </h1>
+     <h1 className="text-3xl font-bold underline border ">Hello world!</h1>
+     <h2>new line added</h2>
     </div>
   );
 }
