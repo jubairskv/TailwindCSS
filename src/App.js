8481a362a8f0ec32b58ac2c401cd250b68619f5d
jubairskv <jubairskv@gmail.com>
2024-03-19 15:44:30 +0530

    new branch created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
      <h1 className="text-3xl font-bold underline border ">Hello world!</h1>
      <h2>new line add to git hub</h2>
       <h3>jubair</h3>
+      <p>frontend branch</p>
     </div>
   );
 }
